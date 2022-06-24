88e9f17811e6104a5e11ce1d20072d70acd6c158
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-06-24 05:24:02 -0400

    tic fully working. better win screen on rps

--- indexJS.js
+++ indexJS.js
@@ -6,19 +6,34 @@ let compCnum = 0;
 let compScore = 0;
 let playerScore = 0;
 
+const winningMessageElement = document.getElementById('winningMessage1');
+const restartButton1 = document.getElementById('restartButton1');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text1]');
+
 // RPS main function, collects all of the functions needed to display evrything on the HTML file
+restartButton1.addEventListener('click', realoadPage)
+
 function RPS() {
-    winner();
     let name = document.getElementById("RPSnameBox").value.toUpperCase();
     //document.getElementById("RPSname").innerHTML = name.toLocaleUpperCase();
     document.getElementById("RPSvs").innerHTML = "VS";
     //document.getElementById("RPSopponent").innerHTML = "COMPUTER";
     playerChoice();
     computerChoice();  
-    document.getElementById("reloadMessage").innerHTML = "(Please reaload the page to play again)";
     document.getElementById("RPSplayerScore").innerHTML = name + ": " + playerScore; 
     document.getElementById("RPScompScore").innerHTML = "COMPUTER:" + compScore;
+    winner();
 }
+
+function RPSrestart() {
+    const compPic = document.getElementById("RPScompPic");
+    const playerPic = document.getElementById("RPSplayerPic");
+
+    compPic.remove();
+    playerPic.remove();
+    winningMessageElement.classList.remove('show')
+}
+
 
 // this sunction get the random number from compNum() function and connects that number to an specif picture
 function computerChoice() {
@@ -62,57 +77,62 @@ function playerChoice() {
 
 // Prompts the user to pic a number that corresponds to an specifit icture and stores it in "playerCnum"
 function playerNum() {
-    while (true) { 
-        this.playerCnum = prompt("What is your choice? (1) rock, (2) paper, (3) scissors: ");
-        if (this.playerCnum < 1 || this.playerCnum > 3) {
+    while (true) {
+        playerCnum = prompt("What is your choice? (1) rock, (2) paper, (3) scissors: ");
+        if (playerCnum < 1 || playerCnum > 3) {
             continue;
         }
-        return this.playerCnum;
+        return playerCnum;
     }
 }
 
 // Randomizes a number between 1 and 3 that decides the picture chose by the computer
 function computerNum() {
-    this.compCnum = Math.floor(Math.random() * 3) + 1
-    return this.compCnum;
+    compCnum = Math.floor(Math.random() * 3) + 1;
+    return compCnum;
 }
 
 // This fuinction decides the winner depending on the number that they chose
 function winner() {
-    let compRand = this.compCnum;
-    let playerRand = this.playerCnum;
-    let playerName = document.getElementById("RPSnameBox").value;
-    let compWin = "The winner is: COMPUTER... Imagine loosing to a CPU, trash can";
-    let playerWin  = "The winner is " + playerName + "!";
-    let tie = "IT'S A TIE!";
+    var playerName = document.getElementById("RPSnameBox").value.toUpperCase();
+    var compWin = "COMPUTER wins!";
+    var playerWin  = playerName + " Wins!";
+    var tie = "Draw!";
 
-    while (true) {
-        if(playerRand == 1 && compRand == 1) {
-            return document.getElementById("RPSwinner").innerHTML = tie;
-        } else if (playerRand == 2 && compRand == 2) {
-            return document.getElementById("RPSwinner").innerHTML = tie;
-        } else if (playerRand == 3 && compRand == 3) {
-            return document.getElementById("RPSwinner").innerHTML = tie;
-        } else if(playerRand == 1 && compRand == 3) {
-            playerScore ++;
-            return document.getElementById("RPSwinner").innerHTML = playerWin;
-        } else if(playerRand == 2 && compRand == 1) {
-            playerScore ++;
-            return document.getElementById("RPSwinner").innerHTML = playerWin;
-        } else if(playerRand == 3 && compRand == 2) {
-            playerScore ++;
-            return document.getElementById("RPSwinner").innerHTML = playerWin;
-        } else {
-            compScore ++;
-            return document.getElementById("RPSwinner").innerHTML = compWin;
-        }
+    
+    if (playerCnum == 1 && compCnum == 3) {
+        playerScore ++;
+        winningMessageTextElement.innerText = playerWin;
+    } else if (playerCnum == 2 && compCnum == 1) {
+        playerScore ++;
+        winningMessageTextElement.innerText = playerWin;
+    } else if (playerCnum == 3 && compCnum == 2) {
+        playerScore ++;
+        winningMessageTextElement.innerText = playerWin;
+    } else if (compCnum == 1 && playerCnum == 3) {
+        compScore ++;
+        winningMessageTextElement.innerText = compWin;
+    } else if (compCnum == 2 && playerCnum == 1) {
+        compScore ++;
+        winningMessageTextElement.innerText = compWin;
+    } else if (compCnum == 3 && playerCnum == 2) {
+        compScore ++;
+        winningMessageTextElement.innerText = compWin;
+    } else {
+        winningMessageTextElement.innerText = tie;
     }
+    winningMessageElement.classList.add("show");
 }
 
 // this function displays the name box and the enter button, it is then to be used once the user chooses tp play RPS
 function showNameBox() {
     document.getElementById("RPSnameBox").style.display="block";
     document.getElementById("RPSEnterButton").style.display="block";
+}
+// removes namebox after it is used
+function removeNameBox() {
+    document.getElementById("RPSnameBox").style.display="none";
+    document.getElementById("RPSEnterButton").style.display="none";
 }
 
 // ************************ END RPS GAME CODE *********************
@@ -132,7 +152,7 @@ function randNum() {
 }
 
 function realoadPage() {
-    return setTimeout(() => window.location.reload(), 2000);
+    return setTimeout(() => window.location.reload(), 0);
 }
 
 function randPic() {
--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,123 @@
+function ticMain() {
+    document.getElementById("ticTitle").style.display="block";
+    // Display board after user picks tictactoe
+    document.getElementById("board").style.display="grid";
+    document.getElementById("cell").style.border = "2px solid black";
+    document.getElementById("cell").style.display = "flex";
+    document.getElementById("cell1").style.border = "2px solid black";
+    document.getElementById("cell1").style.display = "flex";
+    document.getElementById("cell2").style.border = "2px solid black";
+    document.getElementById("cell2").style.display = "flex";
+    document.getElementById("cell3").style.border = "2px solid black";
+    document.getElementById("cell3").style.display = "flex";
+    document.getElementById("cell4").style.border = "2px solid black";
+    document.getElementById("cell4").style.display = "flex";
+    document.getElementById("cell5").style.border = "2px solid black";
+    document.getElementById("cell5").style.display = "flex";
+    document.getElementById("cell6").style.border = "2px solid black";
+    document.getElementById("cell6").style.display = "flex";
+    document.getElementById("cell7").style.border = "2px solid black";
+    document.getElementById("cell7").style.display = "flex";
+    document.getElementById("cell8").style.border = "2px solid black";
+    document.getElementById("cell8").style.display = "flex";
 
-    function ticMain() {
-        // displays the board once user picks tictactoe
-        ticDisplay();
+    const X_CLASS = "x"
+    const CIRCLE_CLASS = "circle"
+    // array of winning combinations
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    // bring all the id's from the HTML file
+    const cellElements = document.querySelectorAll("[data-cell]")
+    const restartButton = document.getElementById("restartButton") 
+    const board = document.getElementById("board")
+    const winningMessageElement = document.getElementById("winningMessage")
+    const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
+    let cirlceTurn
+
+    startGame()
+
+    restartButton.addEventListener("click", startGame)
+
+    function startGame() {
+        cirlceTurn = false
+        cellElements.forEach(cell => {
+            cell.classList.remove(X_CLASS)
+            cell.classList.remove(CIRCLE_CLASS)
+            cell.removeEventListener("click", handleClick)
+            cell.addEventListener("click", handleClick, { once: true })
+        })
+        setBoardHoverClass()
+        winningMessageElement.classList.remove("show")
     }
 
-    function ticDisplay() {
-        document.getElementById("ticBoard").style.display="grid";
-        document.getElementById("ticResetButton").style.display="block";
+    function handleClick(e) {
+        const cell = e.target
+        const currentClass = cirlceTurn ? CIRCLE_CLASS : X_CLASS
+        placeMark(cell, currentClass)
 
+        if (checkWin(currentClass)) {
+            endGame(false)
+        } else if (isDraw()) {
+            endGame(true)
+        } else {
+            swapTurns()
+            setBoardHoverClass()
+        }
     }
 
-   
+    // reaplaces the winning message to show who won
+    function endGame(draw) {
+        if (draw) {
+            winningMessageTextElement.innerText = "Draw!"
+        } else {
+            winningMessageTextElement.innerText = `${cirlceTurn ? "O's" : "X's"} Wins!`
+        }
+        winningMessageElement.classList.add("show")
+    }
+
+    function isDraw() {
+        // turn the cellElements into an array to check if all the cells contain something
+        return [...cellElements].every(cell => {
+            return cell.classList.contains(X_CLASS) || 
+            cell.classList.contains(CIRCLE_CLASS)
+        })
+    }
+
+    function placeMark(cell, currentClass) {
+        cell.classList.add(currentClass)
+    }
+
+    function swapTurns() {
+        cirlceTurn = !cirlceTurn
+    }
+
+    // Adds the hover affect 
+    function setBoardHoverClass() {
+        board.classList.remove(X_CLASS)
+        board.classList.remove(CIRCLE_CLASS)
+        if (cirlceTurn) {
+            board.classList.add(CIRCLE_CLASS)
+        } else {
+            board.classList.add(X_CLASS)
+        }
+    }
+
+     // checks if current class (X or O) is in any of the winning combinations
+    function checkWin(currentClass) {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return cellElements[index].classList.contains(currentClass)
+            })
+        })
+    }
+
+}
 
