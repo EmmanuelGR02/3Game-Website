5a274ff55199507e6997307d51cdcf4a29a80248
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-07-02 01:48:15 -0400

    RPS- fixed main page

--- indexJS.js
+++ indexJS.js
@@ -6,23 +6,16 @@ let compCnum = 0;
 let compScore = 0;
 let playerScore = 0;
 
-const winningMessageElement = document.getElementById('winningMessage1');
-const restartButton1 = document.getElementById('restartButton1');
-const winningMessageTextElement = document.querySelector('[data-winning-message-text1]');
-
 // RPS main function, collects all of the functions needed to display evrything on the HTML file
 restartButton1.addEventListener('click', realoadPage)
 
 function RPS() {
     let name = document.getElementById("RPSnameBox").value.toUpperCase();
-    //document.getElementById("RPSname").innerHTML = name.toLocaleUpperCase();
+    document.getElementById("RPSname").innerHTML = name.toLocaleUpperCase();
     document.getElementById("RPSvs").innerHTML = "VS";
-    //document.getElementById("RPSopponent").innerHTML = "COMPUTER";
-    playerChoice();
-    computerChoice();  
-    document.getElementById("RPSplayerScore").innerHTML = name + ": " + playerScore; 
-    document.getElementById("RPScompScore").innerHTML = "COMPUTER:" + compScore;
-    winner();
+    document.getElementById("RPSopponent").innerHTML = "COMPUTER"; 
+    //document.getElementById("RPSplayerScore").innerHTML = name + ": " + playerScore; 
+    //document.getElementById("RPScompScore").innerHTML = "COMPUTER:" + compScore;
 }
 
 function RPSrestart() {
@@ -59,31 +52,36 @@ function computerChoice() {
 function playerChoice() {
     var img = document.createElement("img");
     var div = document.getElementById("RPSplayerPic");
-    var playerChoice = playerNum();
+    //var playerChoice = playerNum();
 
-    if (playerChoice == 1) {
+    if (playerCnum == 1) {
         img.src = "rock.jpg";
         return div.appendChild(img);
     }
-    else if (playerChoice == 2) {
+    else if (playerCnum == 2) {
         img.src = "paper.jpg";
         return div.appendChild(img);
     }
-    else if (playerChoice == 3) {
+    else if (playerCnum == 3) {
         img.src = "scissors.jpg";
         return div.appendChild(img);
     }
 }
 
-// Prompts the user to pic a number that corresponds to an specifit icture and stores it in "playerCnum"
-function playerNum() {
-    while (true) {
-        playerCnum = prompt("What is your choice? (1) rock, (2) paper, (3) scissors: ");
-        if (playerCnum < 1 || playerCnum > 3) {
-            continue;
-        }
-        return playerCnum;
-    }
+
+function RPSrock() {
+    playerCnum = 1;
+    return playerCnum;
+}
+
+function RPSpaper() {
+    playerCnum = 2;
+    return playerCnum;
+}
+
+function RPSscissors() {
+    playerCnum = 3;
+    return playerCnum;
 }
 
 // Randomizes a number between 1 and 3 that decides the picture chose by the computer
@@ -102,26 +100,25 @@ function winner() {
     
     if (playerCnum == 1 && compCnum == 3) {
         playerScore ++;
-        winningMessageTextElement.innerText = playerWin;
+        alert(playerWin);
     } else if (playerCnum == 2 && compCnum == 1) {
         playerScore ++;
-        winningMessageTextElement.innerText = playerWin;
+        alert(playerWin);
     } else if (playerCnum == 3 && compCnum == 2) {
         playerScore ++;
-        winningMessageTextElement.innerText = playerWin;
+        alert(playerWin);
     } else if (compCnum == 1 && playerCnum == 3) {
         compScore ++;
-        winningMessageTextElement.innerText = compWin;
+        alert(compWin);
     } else if (compCnum == 2 && playerCnum == 1) {
         compScore ++;
-        winningMessageTextElement.innerText = compWin;
+        alert(compWin);
     } else if (compCnum == 3 && playerCnum == 2) {
         compScore ++;
-        winningMessageTextElement.innerText = compWin;
+        alert(compWin);
     } else {
-        winningMessageTextElement.innerText = tie;
+        alert(tie);
     }
-    winningMessageElement.classList.add("show");
 }
 
 // this function displays the name box and the enter button, it is then to be used once the user chooses tp play RPS
@@ -131,65 +128,20 @@ function showNameBox() {
 }
 // removes namebox after it is used
 function removeNameBox() {
+    document.getElementById("rpsNameBox").style.display="none";
     document.getElementById("RPSnameBox").style.display="none";
     document.getElementById("RPSEnterButton").style.display="none";
+    document.getElementById("rpsMakeChoice").style.display="block";
+    document.getElementById("RPSrock").style.display="block";
+    document.getElementById("RPSpaper").style.display="block";
+    document.getElementById("RPSscissors").style.display="block";
 }
 
 // ************************ END RPS GAME CODE *********************
-
-function c4() {
-    var promptName = prompt("Enter your name: ");
-    let name = promptName.toUpperCase();
-    alert("I will now show a pic of " + name + " for 3 seconds ;)");
-
-    randPic();
-    realoadPage();
-}
-
-function randNum() {
-    var randNum = Math.floor(Math.random() * 8) + 1;
-    return randNum;
-}
-
 function realoadPage() {
     return setTimeout(() => window.location.reload(), 0);
 }
 
-function randPic() {
-    var num = randNum();
-
-    var img = document.createElement("img");
-    var div = document.getElementById("funnyPic");
-
-    if (num == 1) {
-        img.src = "monkeyPic.jpg";
-        return  div.appendChild(img);
-    } else if (num == 2) {
-        img.src = "chad.jpg";
-        return div.appendChild(img);
-    } else if (num == 3) {
-        img.src = "beetlejuice.jpg";
-        return  div.appendChild(img);
-    } else if (num == 4) {
-        img.src = "funnyBDude.jpg";
-        return div.appendChild(img);
-    } else if (num == 5) {
-        img.src = "uglyFish.jpg";
-        return div.appendChild(img);
-    } else if (num == 6) {
-        img.src = "uglyMessi.jpg";
-        return div.appendChild(img);
-    } else if (num == 7) {
-        img.src = "uglyRat.jpg";
-        return div.appendChild(img);
-    } else if (num == 8) {
-        img.src = "uglyRonaldo.jpg";
-         return div.appendChild(img);
-    }
-
-}
-
 function dissapear() {
     document.getElementById("hideaway").style.display="none";
-    
 }
